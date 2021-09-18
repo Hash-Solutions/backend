48af892afd9feec461310849377e95e0c7f0bbcf
Marcelo Wischniowski <marcelowisc@gmail.com>
2021-09-18 17:55:37 -0300

    clone

--- src/domain/usecase/account/add-acount.ts
+++
@@ -1,7 +0,0 @@
-import { AccountModel } from "@/domain/models/account";
-
-export type AddAccountParams = Omit<AccountModel, 'id'>;
-
-export interface addAcount {
-  add: (account: AddAccountParams) => Promise<AccountModel>;
-}
--- src/domain/usecase/account/load-account-by-token.ts
+++
@@ -1,5 +0,0 @@
-import { AccountModel } from "@/domain/models/account";
-
-export interface LoadAccountByToken {
-  load: (accessToken: string) => Promise<AccountModel>;
-}
